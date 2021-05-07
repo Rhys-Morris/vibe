798749e98a0fd5ee07b654a978971b110547cafe
Rhys Morris <rhysmorris08@gmail.com>
2021-05-07 11:07:08 +1000

    Added renderCards method

--- app/modules/render.js
+++ app/modules/render.js
@@ -17,3 +17,34 @@ export function renderError(errorMessage) {
 export function clearHtml(domElement) {
   document.querySelector(domElement).innerHTML = "";
 }
+
+function buildArtistsString(artists) {
+  const returnStr = "";
+  artists.forEach((artist, index) => {
+    returnStr +=
+      index === artists.length - 1 ? `${artist.name}` : `${artist.name}, `;
+  });
+  return returnStr;
+}
+
+function renderCards(tracks) {
+  const markup = "";
+  tracks.forEach((track) => {
+    const trackCard = `
+    <div class="playlist__track-container">
+        <div class="playlist__track-card">
+          <img src="${
+            track.album.images[0].url
+          }" alt="band-album-img" class="playlist__track-card__album-image">
+          <h4 class="playlist__track-card__band">${buildArtistsString(
+            track.artists
+          )}</h4>
+          <p class="playlist__track-card__album">${track.album.name}</p>
+          <p class="playlist__track-card__title">${track.name}</p>
+          <div class="playlist__track-card__play-track"><span class="playlist__track-card__play-symbol">▶</span></div>
+        </div>
+    `;
+    markup += trackCard;
+  });
+  document.querySelector(".playlist__track-container");
+}
